c62e542858dbd17f33d2ca3a441206d9871620f2
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-05 20:42:10 +0300

    preparation folders and files in folder components

---
+++ src/components/CartModal/CartModal.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./CartModal.scss"
+
+const CartModal = () => {
+  return <div>CartModal</div>
+}
+
+export default CartModal
---
+++ src/components/Slider/HeaderSlider.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./HeaderSlider.scss"
+
+const HeaderSlider = () => {
+  return <div>HeaderSlider</div>
+}
+
+export default HeaderSlider
